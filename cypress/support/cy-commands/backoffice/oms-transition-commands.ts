9a6543c9b2f68933a92e1e7611b685f6f61597e0
krivtsova <olena.krivtsova@spryker.com>
2025-11-17 15:59:23 +0200

    formatting

--- cypress/support/cy-commands/backoffice/oms-transition-commands.ts
+++ cypress/support/cy-commands/backoffice/oms-transition-commands.ts
@@ -47,12 +47,9 @@ Cypress.Commands.add(
         const baseCommand = path ? `cd ${path} && docker/sdk` : 'docker/sdk'
 
         return cy
-          .exec(
-            `${baseCommand} console oms:check-condition`,
-            {
-              failOnNonZeroExit: true,
-            }
-          )
+          .exec(`${baseCommand} console oms:check-condition`, {
+            failOnNonZeroExit: true,
+          })
           .then((result) => {
             expect(
               result.code,
--- cypress/support/scenarios/backoffice/oms-transition-scenarios.ts
+++ cypress/support/scenarios/backoffice/oms-transition-scenarios.ts
@@ -58,12 +58,9 @@ export class OmsTransitionScenarios {
         const baseCommand = path ? `cd ${path} && docker/sdk` : 'docker/sdk'
 
         return cy
-          .exec(
-            `${baseCommand} console oms:check-condition`,
-            {
-              failOnNonZeroExit: true,
-            }
-          )
+          .exec(`${baseCommand} console oms:check-condition`, {
+            failOnNonZeroExit: true,
+          })
           .then((result) => {
             expect(
               result.code,
